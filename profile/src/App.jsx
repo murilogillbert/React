f8afe56f35c185d269ad8e1772ba8c6d3cf46a2b
murilogillbert <murilo.gillbert@outlook.com>
2024-05-15 15:36:28 -0400

    Pequeno projeto do componente Profile

---
+++ profile/src/App.jsx
@@ -0,0 +1,17 @@
+import Profile from "./Components/Profile";
+
+export default function App(){
+  return (
+    <div className="app">
+      <Profile
+        avatar={"src/assets/a3.webp"}
+        name="Murilo Taques"
+        bio="Full-stack javascript developer"
+        email="murilogillbert@gmail.com"
+        phone="+5565993504640"
+        githubUrl="https://github.com"
+      />
+
+    </div>
+  )
+}
